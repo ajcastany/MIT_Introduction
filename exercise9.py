435f8f2c078a91cb58d0400cdf5121d478c7f17f
Armando Castany <castanymarconiaj1@cardiff.ac.uk>
2018-10-03 16:37:29 +0100

    Exercise 9 now contains two solutions

--- exercise9.py
+++ exercise9.py
@@ -7,11 +7,21 @@ occurs anywhere in the other and False otherwise.
 Hint: use ~str~ built-in operation.
 
 """
-String1 = "asdf"
-String2 = "asdf afdfd"
+String1 = "The blue fox"
+String2 = "The blue"
 
 def StringInString(x, y):
     if x in y or y in x:
+        return True
+    else:
+        return False
+
+# another way of doing this:
+
+def StringLookUP(x, y):
+    if x.__contains__(y):
+        return True
+    elif y.__contains__(x):
         return True
     else:
         return False
