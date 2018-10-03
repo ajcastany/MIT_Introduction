a405b056ce76a145b9bb2f33f926adb461116611
Armando Castany <castanymarconiaj1@cardiff.ac.uk>
2018-10-03 13:14:42 +0100

    Exercise 8 _unsolved_

--- exercise8.py
+++ exercise8.py
@@ -1,8 +1,12 @@
 """Newton Rhapson Method for finding the real roots of many functions,
 This however finds the real root of polynomial with one variable.
 ==========================================================================
-                               VARIABLES
+                               Exercise 8
 ==========================================================================
+Add code to this that keeps track of the number of iterations used to find
+the root.  Use that code as part of a program that compares the efficiency of
+Newton-Rapshon and bisection search.
+
 """
 epsilon  = 0.01
 k = 24.0
